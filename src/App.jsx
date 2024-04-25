e5540492eb356318ac3c58fccacb82fc1036759e
Feniuk <143640539+Feniuk@users.noreply.github.com>
2024-04-25 22:08:52 +0200

    some updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,23 @@ import "./components/App.css";
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactForm from "./components/ContactForm/ContactForm";
-import { useEffect } from "react";
+import { useEffect, lazy } from "react";
 import { fetchContacts } from "./redux/contactsOps";
 import { useDispatch } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import { Suspense } from "react";
+import Loader from "./components/Loader/Loader";
+
+const ContactsPage = lazy(() => import("./pages/ContactsPage.jsx"));
+const HomePage = lazy(() => import("./pages/HomePage.jsx"));
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage.jsx"));
+const LoginPage = lazy(() => import("./pages/LoginPage.jsx"));
+const RegistrationPage = lazy(() => import("./pages/RegistrationPage.jsx"));
 
 function App() {
   const dispatch = useDispatch();
+
+  const isRefreshing = useSelector(selectIsRefreshing);
 
   useEffect(() => {
     dispatch(fetchContacts());
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,21 @@ import App from "./App.jsx";
 import "./index.css";
 import "modern-normalize";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { store, persistor } from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <HelmetProvider>
+      <Provider store={store}>
+        <PersistGate persistor={persistor} loading={null}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
+    </HelmetProvider>
   </React.StrictMode>
 );
